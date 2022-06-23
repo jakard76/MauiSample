1f9c6ad67dbcb0e83e758c5fa701bc59deb5d85e
jakard <jakard@apparelbase.com>
2022-06-23 10:09:43 +0900

    init
    init

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MauiApp1.Pages;
+
+namespace MauiApp1;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		Current.UserAppTheme = AppTheme.Light;
+
+#if WINDOWS || MACCATALYST
+		MainPage = new DesktopShell();
+#else
+		MainPage = new MobileShell();
+#endif
+	}
+}
---
+++ MauiApp1/Pages/DesktopShell.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1.Pages;
+
+public partial class DesktopShell : Shell
+{
+	public DesktopShell()
+	{
+		InitializeComponent();
+	}
+}
