fe4e6dc679f8ece92fce5ea3e25b7fc6f6ad21fc
jakard <jakard@apparelbase.com>
2022-06-24 13:13:33 +0900

    draw test

---
+++ MauiApp1/Graphics/WorkBookDrawable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Graphics
+{
+    internal class WorkbookDrawable : IDrawable
+    {
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            canvas.StrokeColor = Colors.Red;
+            canvas.StrokeSize = 6;
+            canvas.DrawLine(new PointF(20, 20), new PointF(100, 100));
+        }
+    }
+}
